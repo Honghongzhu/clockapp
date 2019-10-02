3a543e67f7ddf9869f9edff40df7d98f8049e608
Honghongzhu <honghong033@hotmail.com>
2019-10-02 22:31:58 +1000

    working buttons 12H/24H
    When clicking on the 12H button, the time will change into a 12-hour format. Similarly, for the 24H button it will change into a 24-hour format

--- app/src/main/java/com/example/clockapp/MainActivity.java
+++ app/src/main/java/com/example/clockapp/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.clockapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextClock;
+
 import java.text.DateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText sydneyTime;
+    private Button twelveHButton;
+    private Button twentyfourHButton;
+    private TextClock clock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ConstraintLayout sydney = findViewById(R.id.city1);
+        clock = sydney.findViewById(R.id.time);
+        clock.setTimeZone();
+
+        twelveHButton = findViewById(R.id.button12H);
+        twelveHButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(clock.is24HourModeEnabled())
+                    clock.setFormat24Hour("hh:mm a");
+                else
+                    clock.setFormat12Hour("hh:mm a");
+            }
+        });
+
+        twentyfourHButton = findViewById(R.id.button24H);
+        twentyfourHButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!clock.is24HourModeEnabled())
+                    clock.setFormat12Hour("hh:mm");
+                else
+                    clock.setFormat24Hour("hh:mm");
+            }
+        });
     }
 }
