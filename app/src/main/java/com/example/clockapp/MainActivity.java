4be6ff886c176f26eb636af9960d36cd7b169080
Honghongzhu <honghong033@hotmail.com>
2019-10-02 21:05:21 +1000

    added textClock
    Changed the textview for the time into a textClock. Changing the API level to 17 was necessary for this as well

--- app/src/main/java/com/example/clockapp/MainActivity.java
+++ app/src/main/java/com/example/clockapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.clockapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText sydneyTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout sydney = findViewById(R.id.city1);
     }
 }
