f3c6ca928b64a3edd992294cd86efd8736673fef
Honghongzhu <honghong033@hotmail.com>
2019-10-03 15:53:48 +1000

    added cities
    The cities new york, london and tokyo are now added

--- app/src/main/java/com/example/clockapp/MainActivity.java
+++ app/src/main/java/com/example/clockapp/MainActivity.java
@@ -3,39 +3,81 @@ package com.example.clockapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextClock;
-
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.TimeZone;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button twelveHButton;
     private Button twentyfourHButton;
-    private TextClock clock;
+    private TextView sydney_name;
+    private TextClock sydney_clock;
+    private ImageView sydney_image;
+    private TextView newyork_name;
+    private TextClock newyork_clock;
+    private ImageView newyork_image;
+    private TextView tokyo_name;
+    private TextClock tokyo_clock;
+    private ImageView tokyo_image;
+    private TextView london_name;
+    private TextClock london_clock;
+    private ImageView london_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ConstraintLayout sydney = findViewById(R.id.city1);
-        clock = sydney.findViewById(R.id.time);
-        clock.setTimeZone("Australia/Sydney");
+        ConstraintLayout sydneyTime = findViewById(R.id.sydneyTime);
+        sydney_name = sydneyTime.findViewById(R.id.city);
+        sydney_name.setText(R.string.Sydney);
+        sydney_clock = sydneyTime.findViewById(R.id.time);
+        sydney_clock.setTimeZone("Australia/Sydney");
+        sydney_image = sydneyTime.findViewById(R.id.image);
+        sydney_image.setImageResource(R.drawable.sydney);
+
+        ConstraintLayout newyorkTime = findViewById(R.id.newyorkTime);
+        newyork_name = newyorkTime.findViewById(R.id.city);
+        newyork_name.setText(R.string.New_York);
+        newyork_clock = newyorkTime.findViewById(R.id.time);
+        newyork_clock.setTimeZone("America/New_York");
+        newyork_image = newyorkTime.findViewById(R.id.image);
+        newyork_image.setImageResource(R.drawable.new_york);
+
+        ConstraintLayout londonTime = findViewById(R.id.londonTime);
+        london_name = londonTime.findViewById(R.id.city);
+        london_name.setText(R.string.London);
+        london_clock = londonTime.findViewById(R.id.time);
+        london_clock.setTimeZone("England/London");
+        london_image = londonTime.findViewById(R.id.image);
+        london_image.setImageResource(R.drawable.london);
+
+        ConstraintLayout tokyoTime = findViewById(R.id.tokyoTime);
+        tokyo_name = tokyoTime.findViewById(R.id.city);
+        tokyo_name.setText(R.string.Tokyo);
+        tokyo_clock = tokyoTime.findViewById(R.id.time);
+        tokyo_clock.setTimeZone("Japan/Tokyo");
+        tokyo_image = tokyoTime.findViewById(R.id.image);
+        tokyo_image.setImageResource(R.drawable.japan);
 
         twelveHButton = findViewById(R.id.button12H);
         twelveHButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(clock.is24HourModeEnabled())
-                    clock.setFormat24Hour("hh:mm a");
+                if (sydney_clock.is24HourModeEnabled()){
+                    sydney_clock.setFormat24Hour("hh:mm a");
+                    newyork_clock.setFormat24Hour("hh:mm a");
+                    london_clock.setFormat24Hour("hh:mm a");
+                    tokyo_clock.setFormat24Hour("hh:mm a");
+                }
                 else
-                    clock.setFormat12Hour("hh:mm a");
+                    sydney_clock.setFormat12Hour("hh:mm a");
+                    newyork_clock.setFormat24Hour("hh:mm a");
+                    london_clock.setFormat24Hour("hh:mm a");
+                    tokyo_clock.setFormat24Hour("hh:mm a");
             }
         });
 
@@ -43,10 +85,10 @@ public class MainActivity extends AppCompatActivity {
         twentyfourHButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!clock.is24HourModeEnabled())
-                    clock.setFormat12Hour("HH:mm");
+                if(!sydney_clock.is24HourModeEnabled())
+                    sydney_clock.setFormat12Hour("HH:mm");
                 else
-                    clock.setFormat24Hour("HH:mm");
+                    sydney_clock.setFormat24Hour("HH:mm");
             }
         });
     }
